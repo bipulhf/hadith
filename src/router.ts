1c5626e97e71c6569ea2a7a159fd6989f905e615
bipulhf <bipulhf@gmail.com>
2024-05-30 18:45:08 +0600

    Updated favourite hadith api

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import db from "../drizzle/db";
-import { hadith } from "../drizzle/schema";
+import { favHadith, hadith } from "../drizzle/schema";
 
 const router = Router();
 
 router.post("/hadith", async (req, res) => {
-  const { description, rabi, book, level } = req.body;
+  const { title, description, rabi, book, level } = req.body;
   try {
     const data = await db
       .insert(hadith)
       .values({
+        title,
         description,
         rabi,
         book,
@@ -40,6 +41,39 @@ router.get("/hadith/:id", async (req, res) => {
       where: (model, { eq }) => eq(model.id, +id),
     });
     return res.status(200).json(data);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ message: "Bad Request" });
+  }
+});
+
+router.get("/hadith/fav/:mobile", async (req, res) => {
+  try {
+    const { mobile } = req.body;
+    const fav = await db.query.favHadith.findMany({
+      where: (model, { eq }) => eq(model.mobile, mobile),
+      with: {
+        hadith: true,
+      },
+    });
+    return res.status(201).json(fav);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ message: "Bad Request" });
+  }
+});
+
+router.post("/hadith/fav", async (req, res) => {
+  try {
+    const { mobile, hadithId } = req.body;
+    const fav = await db
+      .insert(favHadith)
+      .values({
+        hadithId,
+        mobile,
+      })
+      .returning();
+    return res.status(201).json(fav);
   } catch (e) {
     console.log(e);
     return res.status(400).json({ message: "Bad Request" });
